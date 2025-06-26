280da8b9f1a4dd1dea9cb5704eb2271742b9421b
Riquelmy Martins <72394084+Riquelmy247@users.noreply.github.com>
2025-06-25 23:36:34 -0300

    Fotos

--- fotos/js/script.js
+++ fotos/js/script.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     
     function loadFolders() {
         const folders = [
+            { title: 'Campanha - Fé e Milagre', date: '25/06/2025 | Quarta', path: 'fotos/25-06 QUARTA' },
+            { title: 'Série - A Igreja de Cristo', date: '22/06/2025 | Noite', path: 'fotos/22-06 NOITE' },
+            { title: 'Série - A Igreja de Cristo', date: '22/06/2025 | Manhã', path: 'fotos/22-06 MANHÃ' },
             { title: 'Série - A Igreja de Cristo', date: '15/06/2025 | Noite', path: 'fotos/15-06 NOITE' },
             { title: 'Série - A Igreja de Cristo', date: '15/06/2025 | Manhã', path: 'fotos/15-06 MANHÃ' },
             { title: 'Série - A Igreja de Cristo', date: '08/06/2025 | Noite', path: 'fotos/08-06 NOITE' },
